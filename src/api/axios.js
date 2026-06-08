faf49e04a9de11c0cdc31d8941adc243393ee060
gayansp <gayanlakshithasp@gmail.com>
2026-06-08 17:08:23 +0530

    railway add

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://srilanka-tours-backend.vercel.app/api',
+    baseURL: 'https://srilanka-tours-backend-production.up.railway.app/api',
 });
 
 api.interceptors.request.use((config) => {
