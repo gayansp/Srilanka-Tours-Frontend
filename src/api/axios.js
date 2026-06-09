916b2cd75f7acaf11fd3a58eb2ab00e3e396f8bd
gayansp <gayanlakshithasp@gmail.com>
2026-06-09 15:42:18 +0530

    vps host added

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://srilanka-tours-backend-production.up.railway.app/api',
+    baseURL: 'api.udawalawatours.com/api',
 });
 
 api.interceptors.request.use((config) => {
